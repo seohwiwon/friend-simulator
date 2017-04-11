d2f8d55f58d69ec3164cd1769489e750f653d104
Seo Hwiwon <tjgnldnjs0606@naver.com>
2017-04-11 20:12:11 +0900

    #9 버튼 여러개를 onClick()에서 모두 처리하게 하기

--- app/src/main/java/kr/hs/emirim/gnldnjs66/sodam/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/gnldnjs66/sodam/MainActivity.java
@@ -11,7 +11,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    void onClick(View view){
-        Toast.makeText(this, "작동 해요!", Toast.LENGTH_SHORT).show(); //.show()를 해야 Toast가 실행된다.
+    void onClick(View view) {
+         switch (view.getId()) {
+             case R.id.button1:
+                 Toast.makeText(this, "아 집가고 싶어", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+             case R.id.button2:
+                 Toast.makeText(this, "하 망했다", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+             case R.id.button3:
+                 Toast.makeText(this, "졸려", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+             case R.id.button4:
+                 Toast.makeText(this, "나 자바 좀 알려줘라", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+             case R.id.button5:
+                 Toast.makeText(this, "하잇", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+             default:
+                 Toast.makeText(this, "으에에에ㅔㄱ!!!", Toast.LENGTH_SHORT).show();
+                 break; //.show()를 해야 Toast가 실행된다.
+                   }
     }
 }
